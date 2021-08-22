09e18c962a2d12258989ed3fdd1d42e98cc3d294
sgopalam <gopalamsai14@gmail.com>
2021-08-21 23:50:08 -0500

    Junit first push init

---
+++ src/test/java/junitpracticemockito/ISBNValidatorTest.java
@@ -0,0 +1,14 @@
+package junitpracticemockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ISBNValidatorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
